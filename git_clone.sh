a834fc08319ad072e9d993d97a1c60f258a27eca
Badrbnh <Badrechahide@gmail.com>
2023-03-19 00:34:26 +0100

    {git clone script}

---
+++ git_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+printf "\n"
+read -p "What's your token? : " token
+printf "\n"
+read -p "What's your username? : " username
+printf "\n"
+read -p "What's your repository name? : " repo_name
+printf "\n"
+git clone https://"${token}"@github.com/"${username}"/"${repo_name}".git
