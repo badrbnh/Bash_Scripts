fc955890fbe31b7ac01e8402ae3affc4bf4afac3
Badrbnh <Badrechahide@gmail.com>
2023-03-19 00:14:20 +0100

    {Add comments}

--- FileFormat.sh
+++ FileFormat.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -iname '*.c' -exec bash -c 'dos2unix {}'  \;
+find . -iname '*.c' -exec bash -c 'dos2unix {}'  \; #Finds all C files and convert them to unix file format.
--- git.sh
+++ git.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-read -p "What's your meassage: " message
-git add . && git commit -m "{$message}" && git push
+read -p "What's your meassage: " message #get's the message that the user want to put in the commits
+
+git add . && git commit -m "{$message}" && git push #add all files and commit them using message variable and push them.
